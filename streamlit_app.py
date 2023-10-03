2fcc16940f5c0ccb70472734f2c3d27c7ceabbbe
m-odaybat <146748925+m-odaybat@users.noreply.github.com>
2023-10-03 15:33:10 +0100

    Update streamlit_app.py
    added selected_fruit variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show) 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())# just writes the data to t he screen
+selected_fruit = "kiwi"
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + selected_fruit)
+# streamlit.text(fruityvice_response.json())# just writes the data to t he screen
 
 #takes the json version of the response and normalizes it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
