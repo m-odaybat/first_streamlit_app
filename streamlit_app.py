8078ae244fbe94cbc8e615473c647959229cb594
m-odaybat <146748925+m-odaybat@users.noreply.github.com>
2023-10-04 12:37:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,13 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show) 
 
+def get_fruity_vice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  #takes the json version of the response and normalizes it
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return  fruityvice_normalized 
+  
+
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -36,12 +43,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-
-def get_fruity_vice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  #takes the json version of the response and normalizes it
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return  fruityvice_normalized 
   
 streamlit.stop()
 # snowflake connector part
